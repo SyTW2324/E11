464f503923b3bbbdd62bef077e37eff91a1731cc
FebeFarina <alu0101430973@ull.edu.es>
2023-11-06 20:09:35 +0000

    Logos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Guess from './components/Guess';
-import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
