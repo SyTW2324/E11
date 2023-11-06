23a68c13861005365c4cad0abcef8a13d5fd488f
FebeFarina <alu0101430973@ull.edu.es>
2023-11-06 20:41:09 +0000

    Categorias como array

--- src/components/Guess.tsx
+++ src/components/Guess.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
+import { useState } from 'react';
 import Category from './Category';
 import '../styles/Guess.css';
 
+
 function Guess() {
+  const [categories, setCategories] = useState(
+    [
+      { title: 'Clase', category: 'Mamífero' },
+      { title: 'Peso', category: '6000 kg' },
+      { title: 'Altura', category: '3,3 m' },
+      { title: 'Hábitat', category: 'Sabana' },
+      { title: 'Medio', category: 'Terrestre' }
+    ]
+  );
   return (
     <div className='guess-container'>
       <div className='guess-image'>
         <img src='https://static.nationalgeographic.es/files/styles/image_3200/public/01-orphan-elephants-nationalgeographic_2188826.jpg?w=1600&h=1067' alt='Elefante' />
       </div>
       <div className='guess-categories'>
-        <Category title='Clase' category='Mamífero' />
-        <Category title='Peso' category='6000 kg' />
-        <Category title='Altura' category='3,3 m' />
-        <Category title='Hábitat' category='Sabana' />
-        <Category title='Medio' category='Terrestre' />
+        {
+          categories.map((category, index) => {
+            return (
+              <Category key={index} title={category.title} category={category.category} />
+            )
+          })
+        }
       </div>
     </div>
   )
