bff15fe20a1e92dbdd506c6f33da0616861d8789
Febe <alu0101430973@alu.edu.es>
2023-11-06 15:42:35 +0000

    Jugando con React

---
+++ src/components/Category.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface GuessProps {
+  title: string;
+  category: string;
+}
+
+function Category(props: GuessProps) {
+  return (
+    <div className='category-container'>
+      <p> {props.title} </p>
+      <p> {props.category} </p>
+    </div>
+  )
+}
+
+export default Category;
---
+++ src/components/Guess.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Category from './Category.tsx';
+
+function Guess() {
+  return (
+    <div className='guess-container'>
+      <div className='guess-image'>
+        <img src='https://static.nationalgeographic.es/files/styles/image_3200/public/01-orphan-elephants-nationalgeographic_2188826.jpg?w=1600&h=1067' alt='Elefante' />
+      </div>
+      <div className='guess-categories'>
+        <Category title='Hola' category='Si' />
+        <Category title='Hola' category='Si' />
+        <Category title='Hola' category='Si' />
+        <Category title='Hola' category='Si' />
+        <Category title='Hola' category='Si' />
+      </div>
+    </div>
+  )
+}
+
+export default Guess;
