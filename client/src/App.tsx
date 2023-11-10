49680ada9c99640ce2db0f697dee79d6737d5ca0
Febe <alu0101430973@alu.edu.es>
2023-11-10 12:17:05 +0000

    Redux

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 import "./App.css";
 import Guess from "./components/Guess";
 import GuessList from "./components/GuessList";
---
+++ client/src/redux/reducers.ts
@@ -0,0 +1,51 @@
+const initialState = {
+  user: {},
+  isAuthenticated: false,
+  isLoading: false,
+}
+
+const reducer = (state = initialState, action: any) => {
+  switch (action.type) {
+    case 'USER_LOADING':
+      return {
+        ...state,
+        isLoading: true
+      }
+    case 'USER_LOADED':
+      return {
+        ...state,
+        isAuthenticated: true,
+        isLoading: false,
+        user: action.payload
+      }
+    case 'LOGIN_SUCCESS':
+    case 'REGISTER_SUCCESS':
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: true,
+        isLoading: false
+      }
+    case 'LOGOUT_SUCCESS':
+      return {
+        ...state,
+        user: {},
+        isAuthenticated: false,
+        isLoading: false
+      }
+    case 'AUTH_ERROR':
+    case 'LOGIN_FAIL':
+    case 'LOGOUT_SUCCESS':
+    case 'REGISTER_FAIL':
+      return {
+        ...state,
+        user: {},
+        isAuthenticated: false,
+        isLoading: false
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer
