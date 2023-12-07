424b0d38e506b6c746ad7da1ae14bf1c6745bbab
Daniel Glez de Chaves González <72324395+DanielGlezDeChavesGonzalez@users.noreply.github.com>
2023-12-07 15:41:41 +0000

    test

--- client/src/tests/home.test.ts
+++ client/src/tests/home.test.ts
@@ -1,8 +1,21 @@
-import puppeteer from "puppeteer";
-import { Browser, Page } from "puppeteer";
-import App from "../App";
+import supertest from 'supertest';
+import puppeteer, { Browser, Page } from 'puppeteer';
+import {describe, expect, test} from '@jest/globals';
 
 describe("home", () => {
+  
+      let browser: Browser;
+      let page: Page;
+  
+      beforeAll(async () => {
+
+        
+    });
+
+  
+      it("should load home page", async () => {
+
+      });
 
 });
 
