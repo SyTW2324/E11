eb341abf1334acbb5c9303a412365efc2a5d31d1
Daniel Glez de Chaves González <72324395+DanielGlezDeChavesGonzalez@users.noreply.github.com>
2023-12-07 16:11:27 +0000

    funciona1

--- server/src/tests/routes.test.ts
+++ server/src/tests/routes.test.ts
@@ -8,80 +8,82 @@ import path from "path";
 
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
-beforeAll(async () => {
-  const res = await request(app).post("/register").send({
-    username: "test",
-    password: "test",
-    email: "test@gmail.com",
-  });
-});
-
-describe("user routes", () => {
-  it("should get all users", async () => {
-    const res = await request(app).get("/user");
-    expect(res.status).to.equal(200);
-    expect(res.body).to.be.an("array");
-  });
-
-  it("should delete a user", async () => {
-    const res = await request(app).delete("/user").send({
-      username: "test",
-      password: "test",
-      email: "test@gmail.com",
-    });
-    expect(res.status).to.equal(200);
-    expect(res.body).to.be.an("object");
-  });
-});
-
-describe("animal routes", () => {
-  it("should get all animals", async () => {
-    const res = await request(app).get("/animals");
-    expect(res.status).to.equal(200);
-    expect(res.body).to.be.an("array");
-  });
-
-  it("should get a random animal", async () => {
-    const res = await request(app).get("/animals/random");
-    expect(res.status).to.equal(200);
-    expect(res.body).to.be.an("object");
-  });
-
-  it("should get an animal by name", async () => {
-    const res = await request(app).get("/animals/name/Serpiente");
-    expect(res.status).to.equal(200);
-    expect(res.body).to.be.an("object");
-  });
-});
-
-describe("register routes", () => {
-  it("should register a new user", async () => {
+describe("server tests", () => {
+  beforeAll(async () => {
     const res = await request(app).post("/register").send({
       username: "test",
       password: "test",
       email: "test@gmail.com",
     });
-    expect(res.status).to.equal(200);
-    expect(res.body).to.be.an("object");
   });
-});
 
-describe("login routes", () => {
-  it("should login a user", async () => {
-    const res = await request(app).post("/login").send({
+  context("user routes", () => {
+    it("should get all users", async () => {
+      const res = await request(app).get("/user");
+      expect(res.status).to.equal(200);
+      expect(res.body).to.be.an("array");
+    });
+
+    it("should delete a user", async () => {
+      const res = await request(app).delete("/user").send({
+        username: "test",
+        password: "test",
+        email: "test@gmail.com",
+      });
+      expect(res.status).to.equal(200);
+      expect(res.body).to.be.an("object");
+    });
+  });
+
+  context("animal routes", () => {
+    it("should get all animals", async () => {
+      const res = await request(app).get("/animals");
+      expect(res.status).to.equal(200);
+      expect(res.body).to.be.an("array");
+    });
+
+    it("should get a random animal", async () => {
+      const res = await request(app).get("/animals/random");
+      expect(res.status).to.equal(200);
+      expect(res.body).to.be.an("object");
+    });
+
+    it("should get an animal by name", async () => {
+      const res = await request(app).get("/animals/name/Serpiente");
+      expect(res.status).to.equal(200);
+      expect(res.body).to.be.an("object");
+    });
+  });
+
+  context("register routes", () => {
+    it("should register a new user", async () => {
+      const res = await request(app).post("/register").send({
+        username: "test",
+        password: "test",
+        email: "test@gmail.com",
+      });
+      expect(res.status).to.equal(200);
+      expect(res.body).to.be.an("object");
+    });
+  });
+
+  context("login routes", () => {
+    it("should login a user", async () => {
+      const res = await request(app).post("/login").send({
+        password: "test",
+        email: "test@gmail.com",
+      });
+      expect(res.status).to.equal(200);
+      expect(res.body).to.be.an("object");
+    });
+  });
+
+  afterAll(async () => {
+    const res = await request(app).delete("/user").send({
+      username: "test",
       password: "test",
       email: "test@gmail.com",
     });
-    expect(res.status).to.equal(200);
-    expect(res.body).to.be.an("object");
+    mongoose.connection.close();
   });
-});
-
-afterAll(async () => {
-  const res = await request(app).delete("/user").send({
-    username: "test",
-    password: "test",
-    email: "test@gmail.com",
-  });
-  mongoose.connection.close();
 });
