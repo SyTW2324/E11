f7110036d488f967d7fb6f367f2eef097d9f1799
FebeFarina <alu0101430973@ull.edu.es>
2023-12-20 20:45:28 +0000

    Delete test users

--- client/src/tests/register.test.ts
+++ client/src/tests/register.test.ts
@@ -1,60 +1,72 @@
-import supertest from 'supertest';
-import puppeteer, { Browser, Page } from 'puppeteer';
-import {describe, expect, test} from '@jest/globals';
+import supertest from "supertest";
+import puppeteer, {Browser, Page} from "puppeteer";
+import {describe, expect, test} from "@jest/globals";
 
 describe("Register", () => {
+  let browser: Browser;
+  let page: Page;
 
-    let browser: Browser;
-    let page: Page;
+  beforeAll(async () => {
+    const request = supertest("http://localhost:5000");
+    const response = await request.get("/user/name/usertest");
+    if (response.status === 200) {
+      await request.delete(`/user/${response.body._id}`);
+    }
 
-    beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+  });
 
-        const request = supertest('http://localhost:5000');
-        const response = await request.get('/user/name/usertest');
-        if (response.status === 200) {
-            await request.delete(`/user/${response.body._id}`);
-        }
+  afterAll(async () => {
+    await browser.close();
+    const request = supertest("http://localhost:5000");
+    const response = await request.get("/user/name/usertest");
+    if (response.status === 200) {
+      await request.delete(`/user/${response.body._id}`);
+    }
+  });
 
-        browser = await puppeteer.launch();
-        page = await browser.newPage();
-    });
-
-    it("should register", async () => {
-        await page.goto("http://localhost:3000/register");
-        await page.waitForSelector('input[name="username"]');
-        await page.type('input[name="username"]', "usertest");
-        await page.type('input[name="email"]', "test@gmail.com");
-        await page.type('input[name="password"]', "test");
-        await page.click('button[id="register-button"]');
-    });
-    it("should not register - user already registered", async () => {
-        await page.goto("http://localhost:3000/register");
-        await page.waitForSelector('input[name="username"]');
-        await page.type('input[name="username"]', "fubo");
-        await page.type('input[name="email"]', "fubo@gmail.com");
-        await page.type('input[name="password"]', "fubo");
-        await page.click('button[id="register-button"]');
-        await page.waitForSelector('p[id="register-error"]');
-        const error = await page.$eval('p[id="register-error"]', (e) => e.textContent);
-        expect(error).toBe("Request failed with status code 409");
-    });
-    it("should not register - email format", async () => {
-        await page.goto("http://localhost:3000/register");
-        await page.waitForSelector('input[name="username"]');
-        await page.type('input[name="username"]', "usertest");
-        await page.type('input[name="email"]', "test");
-        await page.type('input[name="password"]', "test");
-        await page.click('button[id="register-button"]');
-        const url = await page.url();
-        expect(url).toBe("http://localhost:3000/register");
-    });
-    it("should not register - empty form", async () => {
-        await page.goto("http://localhost:3000/register");
-        await page.waitForSelector('input[name="username"]');
-        await page.click('button[id="register-button"]');
-        await page.waitForSelector('p[id="register-error"]');
-        const error = await page.$eval('p[id="register-error"]', (e) => e.textContent);
-        expect(error).toBe("Request failed with status code 422");
-
-    });
+  it("should register", async () => {
+    await page.goto("http://localhost:3000/register");
+    await page.waitForSelector('input[name="username"]');
+    await page.type('input[name="username"]', "usertest");
+    await page.type('input[name="email"]', "test@gmail.com");
+    await page.type('input[name="password"]', "test");
+    await page.click('button[id="register-button"]');
+  });
+  it("should not register - user already registered", async () => {
+    await page.goto("http://localhost:3000/register");
+    await page.waitForSelector('input[name="username"]');
+    await page.type('input[name="username"]', "fubo");
+    await page.type('input[name="email"]', "fubo@gmail.com");
+    await page.type('input[name="password"]', "fubo");
+    await page.click('button[id="register-button"]');
+    await page.waitForSelector('p[id="register-error"]');
+    const error = await page.$eval(
+      'p[id="register-error"]',
+      (e) => e.textContent
+    );
+    expect(error).toBe("Request failed with status code 409");
+  });
+  it("should not register - email format", async () => {
+    await page.goto("http://localhost:3000/register");
+    await page.waitForSelector('input[name="username"]');
+    await page.type('input[name="username"]', "usertest");
+    await page.type('input[name="email"]', "test");
+    await page.type('input[name="password"]', "test");
+    await page.click('button[id="register-button"]');
+    const url = await page.url();
+    expect(url).toBe("http://localhost:3000/register");
+  });
+  it("should not register - empty form", async () => {
+    await page.goto("http://localhost:3000/register");
+    await page.waitForSelector('input[name="username"]');
+    await page.click('button[id="register-button"]');
+    await page.waitForSelector('p[id="register-error"]');
+    const error = await page.$eval(
+      'p[id="register-error"]',
+      (e) => e.textContent
+    );
+    expect(error).toBe("Request failed with status code 422");
+  });
 });
