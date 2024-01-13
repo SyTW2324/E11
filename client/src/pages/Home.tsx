ddf74d809aee73e696c14d2ef3bde50cfd563493
Daniel Glez de Chaves González <72324395+DanielGlezDeChavesGonzalez@users.noreply.github.com>
2024-01-13 17:13:14 +0000

    hay que logear para entrar

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,9 +3,46 @@ import GuessList from "../components/GuessList";
 import Footer from "../components/Footer";
 import {useSelector} from "react-redux";
 import "../styles/Home.css";
+import {useEffect} from "react";
+import {useDispatch} from "react-redux";
+import {getUser} from "../slices/authSlice";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 function Home() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const auth = useSelector((state: any) => state.auth);
+
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+    friends: [],
+    points: 0,
+  });
+
+  useEffect(() => {
+    if (!auth._id) {
+      navigate("/login");
+    }
+  }, [auth._id, navigate]);
+
+  useEffect(() => {
+    if (auth._id) {
+      dispatch(getUser() as any);
+    }
+  }, [auth._id, dispatch]);
+
+  useEffect(() => {
+    setUser({
+      username: auth.username,
+      email: auth.email,
+      password: "",
+      friends: auth.friends,
+      points: auth.points,
+    });
+  }, [auth.username, auth.email]);
 
   console.log(auth);
   return (
--- client/src/pages/User.tsx
+++ client/src/pages/User.tsx
@@ -1,12 +1,12 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import {useSelector} from "react-redux";
-import {useEffect} from "react";
-import {useDispatch} from "react-redux";
-import {getUser} from "../slices/authSlice";
-import {useState} from "react";
-import {useNavigate} from "react-router-dom";
-import {toast} from "react-toastify";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getUser } from "../slices/authSlice";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function User() {
   const dispatch = useDispatch();
@@ -58,15 +58,17 @@ function User() {
             <div className="grid grid-cols-3 text-center order-last md:order-first mt-20 md:mt-0">
               <div className="bg-burlywood rounded-full">
                 <p className="font-bold text-gray-700 text-xl">{user.points}</p>
+                <p className="text-gray-400">Puntos</p>
+              </div>
+              <div className="bg-burlywood rounded-full">
+                <p className="font-bold text-gray-700 text-xl">
+                  {user.friends.length}
+                </p>
                 <p className="text-gray-400">Amigos</p>
               </div>
               <div className="bg-burlywood rounded-full">
-                <p className="font-bold text-gray-700 text-xl">10</p>
+                <p className="font-bold text-gray-700 text-xl">0</p>
                 <p className="text-gray-400">Ranking mundial</p>
-              </div>
-              <div className="bg-burlywood rounded-full">
-                <p className="font-bold text-gray-700 text-xl">89</p>
-                <p className="text-gray-400">Ranking amigos</p>
               </div>
             </div>
             <div className="relative">
@@ -75,7 +77,8 @@ function User() {
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-24 w-24"
                   viewBox="0 0 20 20"
-                  fill="currentColor">
+                  fill="currentColor"
+                >
                   <path
                     fill-rule="evenodd"
                     d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
